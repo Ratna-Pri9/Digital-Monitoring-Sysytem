478820f175d49bb88650069180b055fc7d4addbc
Ratna-Pri9 <ratnaapriyaa009@gmail.com>
2024-10-04 13:25:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+db_path = 'F:/MCA PROJECT/Final/Data/university.db'
+sp_path="F:/MCA PROJECT/Final/Data/shape_predictor_68_face_landmarks.dat"
+facerec_path="F:/MCA PROJECT/Final/Data/dlib_face_recognition_resnet_model_v1.dat"
+save_directory1 = "F:/MCA PROJECT/Final/Data/AttenImages"
+class_state="F:/MCA PROJECT/Final/Data/class_state.json"
+saved_faces_directory1 = 'F:/MCA PROJECT/Final/Data/SavedFaces'
